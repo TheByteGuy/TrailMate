591bf3e4e72f69433e069d4f3057f9e144c24778
Dathan Lang <dathan_lang@outlook.com>
2026-02-28 13:28:55 -0500

    added fix for setup
    installed nodemon dependencies
    0.0.0.0 fixes issue with vsc not allowing us to see website

--- server.js
+++ server.js
@@ -242,6 +242,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`\n🐓 Blue Hen Walks running at http://localhost:${PORT}\n`);
 });
